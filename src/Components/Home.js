37d92b598a77c45511609fad47bceba32df2f06f
aleikson <aleiksonsilva@hotmail.com>
2023-03-04 00:58:43 -0300

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,31 @@
+import React from "react";
+import BannerImage from "../Assets/home-banner-image.png";
+import Navbar from "./Navbar";
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <Navbar />
+            <div className="home-banner-container">
+                <div className="home-bannerImage-container">
+                </div>
+                <div className="home-text-section">
+                    <h1 className="primary-heading">
+                        Your journey starts now
+                    </h1>
+                    <p className="primary-text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, sequi.
+                    </p>
+                    <button className="secondary-button">
+                        Read More
+                    </button>
+                </div>
+                <div className="home-image-section">
+                    <img src={BannerImage} alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Navbar = () => {
+
+    return (
+        <nav>
+            <div className="nav-logo-container">
+                JOURNEYLAND
+            </div>
+            <div className="navbar-links-container">
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Features</a>
+            </div>
+            <div className="navbar-menu-container">
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
