2e954e4a28584e8d21391919eb5492e366ad16cf
aleikson <aleiksonsilva@hotmail.com>
2023-03-04 15:13:12 -0300

    Added Scroll

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,19 +1,24 @@
 import React from "react";
 
 const Navbar = () => {
+    const scrollToSection = (event, sectionId) => {
+        event.preventDefault();
+        const section = document.getElementById(sectionId);
+        if (section) {
+            const top = section.offsetTop;
+            window.scrollTo({ top, behavior: "smooth" });
+        }
+    };
 
     return (
         <nav>
-            <div className="nav-logo-container">
-                JOURNEYLAND
-            </div>
+            <div className="nav-logo-container">JOURNEYLAND</div>
             <div className="navbar-links-container">
-                <a href="">Home</a>
-                <a href="">About</a>
-                <a href="">Features</a>
+                <a href="#home" onClick={(e) => scrollToSection(e, "home")}>Home</a>
+                <a href="#about" onClick={(e) => scrollToSection(e, "about")}>About</a>
+                <a href="#features" onClick={(e) => scrollToSection(e, "features")}>Features</a>
             </div>
-            <div className="navbar-menu-container">
-            </div>
+            <div className="navbar-menu-container"></div>
         </nav>
     );
 };
